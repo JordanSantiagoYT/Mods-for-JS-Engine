003f72c6a8a5dd0f2af001565b0d0855369ba04e
JordanSantiagoYT <jordan.doesosu2@gmail.com>
2024-03-05 21:49:43 -0500

    ME HEAD!!
    Added Head Hurting Juice

--- funnysongs/data/hahahahahahahahaha/loopScript.lua
+++ funnysongs/data/hahahahahahahahaha/loopScript.lua
@@ -4,6 +4,6 @@ end
 function onBeatHit()
 	if curBeat == 116 then
 		loopTheSong(34000)
-		setProperty('polyphony', getProperty('polyphony') * 2)
+		setProperty('polyphony', polyphony * 2)
 	end
 end
