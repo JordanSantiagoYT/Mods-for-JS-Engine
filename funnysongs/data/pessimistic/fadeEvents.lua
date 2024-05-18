c87f5e175856a2c516629c28b24c8350738edeb0
JordanSantiagoYT <jordan.doesosu2@gmail.com>
2024-05-18 03:02:26 -0400

    pemsimistic

---
+++ funnysongs/data/pessimistic/fadeEvents.lua
@@ -0,0 +1,22 @@
+function onCreatePost()
+	makeLuaSprite('flash', '', 0, 0);
+	makeGraphic('flash', 1280, 720, '000000')
+	addLuaSprite('flash', true);
+	setLuaSpriteScrollFactor('flash', 0, 0)
+	setProperty('flash.scale.x', 2)
+	setProperty('flash.scale.y', 2)
+	setProperty('flash.alpha', 1)
+	setObjectCamera('flash', 'camOther')
+end
+
+function onSongStart()
+	doTweenAlpha('flTw', 'flash', 0, 8, 'linear')
+end
+
+local fadedOutYet = false
+function onStepHit()
+	if curStep >= 4608 and not fadedOutYet then
+		fadedOutYet = true
+		doTweenAlpha('flashIn', 'flash', 1, 10, 'linear')
+	end
+end
