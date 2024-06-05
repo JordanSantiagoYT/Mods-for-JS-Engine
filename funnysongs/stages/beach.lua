c7ad0633f6822de3c647d59cbbf837d110c39244
JordanSantiagoYT <jordan.doesosu2@gmail.com>
2024-06-04 22:17:17 -0400

    screwointy

---
+++ funnysongs/stages/beach.lua
@@ -0,0 +1,30 @@
+--created with Super_Hugo's Stage Editor v1.6.3
+
+function onCreate()
+
+	makeLuaSprite('obj3', 'screwedCoin/sky', -368, -119)
+	setObjectOrder('obj3', 0)
+	scaleObject('obj3', 1.7, 1.7)
+	addLuaSprite('obj3', true)
+	
+	makeLuaSprite('obj4', 'screwedCoin/clouds', -370, -34)
+	setObjectOrder('obj4', 1)
+	scaleObject('obj4', 1.8, 1.8)
+	addLuaSprite('obj4', true)
+	
+	makeLuaSprite('obj13', 'screwedCoin/bush', -424, 326)
+	setObjectOrder('obj13', 2)
+	scaleObject('obj13', 1.7, 1.7)
+	addLuaSprite('obj13', true)
+	
+	makeLuaSprite('obj14', 'screwedCoin/palm trees', -53, 62)
+	setObjectOrder('obj14', 2)
+	scaleObject('obj14', 1.7, 1.7)
+	addLuaSprite('obj14', true)
+	
+	makeLuaSprite('obj16', 'screwedCoin/sand', -510, 532)
+	setObjectOrder('obj16', 4)
+	scaleObject('obj16', 1.7, 1.7)
+	addLuaSprite('obj16', true)
+	
+end
