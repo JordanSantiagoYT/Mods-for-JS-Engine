84544635bc241c8b9ca80de046a0f63702c300c0
JordanSantiagoYT <jordan.doesosu2@gmail.com>
2024-07-03 04:22:07 -0400

    add epitome + remove a minor
    galaxy fucking lied

--- funnysongs/stages/moonYT.lua
+++
@@ -1,8 +0,0 @@
-function onCreate()
-	-- background
-	
-	makeLuaSprite('theStage', 'groomerBG', 50, 260);
-	setScrollFactor('theStage', 1, 1);
-	scaleObject('theStage', 1.7, 1.7);
-	addLuaSprite('theStage', false);
-end
