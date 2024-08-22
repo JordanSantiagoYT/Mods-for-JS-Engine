63be63e9742ba42af3245363a3dbbe279aa6b951
JordanSantiagoYT <jordan.doesosu2@gmail.com>
2024-08-22 00:40:17 -0400

    The Eye of God (V2)
    after about 2 years i chart this LOL

--- JS Bambi Fansong Charts/custom_events/Flash Camera.lua
+++ JS Bambi Fansong Charts/custom_events/Flash Camera.lua
@@ -4,13 +4,14 @@ function onEvent(n,v1,v2)
 	if n == 'Flash Camera' then
 
 	   makeLuaSprite('flashBang', '', 0, 0);
-        makeGraphic('flashBang',1280,720,'ffffff')
+       	makeGraphic('flashBang',1280,720,'ffffff')
 	      addLuaSprite('flashBang', true);
 	      setLuaSpriteScrollFactor('flashBang',0,0)
 	      setProperty('flashBang.scale.x',2)
 	      setProperty('flashBang.scale.y',2)
 	      setProperty('flashBang.alpha',0)
 		setProperty('flashBang.alpha',1)
+		setObjectCamera('flashBang', 'camHUD')
 		doTweenAlpha('flashingBye','flashBang',0,v1,'linear')
 	end
 
