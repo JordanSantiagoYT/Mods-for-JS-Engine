545ed9aa060008c3bc370a6b68d0f3d80ed5b3eb
JordanSantiagoYT <jordan.doesosu2@gmail.com>
2024-10-03 09:06:07 -0400

    red ahh cube :sob:

---
+++ JS Bambi Fansong Charts/data/small-hen/fadeEvents.lua
@@ -0,0 +1,20 @@
+function onCreatePost()
+	makeLuaSprite('flash', '', 0, 0);
+	makeGraphic('flash', 1280, 720, '000000')
+	addLuaSprite('flash', true);
+	setLuaSpriteScrollFactor('flash', 0, 0)
+	setProperty('flash.scale.x', 2)
+	setProperty('flash.scale.y', 2)
+	setProperty('flash.alpha', 1)
+	setObjectCamera('flash', 'camOther')
+end
+
+function onSongStart()
+	doTweenAlpha('flTw', 'flash', 0, 6.5, 'linear')
+end
+
+function onStepHit()
+	if curStep == 2402 then
+		doTweenAlpha('okBye', 'flash', 1, 0.001, 'linear')
+	end
+end
