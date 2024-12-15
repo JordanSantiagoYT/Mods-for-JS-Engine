9b89b674508125789fd8af26465973b4074d7e51
JordanSantiagoYT <jordan.doesosu2@gmail.com>
2024-12-15 02:30:56 -0500

    better bg for Sarvatrika Atmahatya

--- JS Bambi Fansong Charts/stages/respace.lua
+++ JS Bambi Fansong Charts/stages/respace.lua
@@ -2,6 +2,11 @@ function onCreate()
 	makeLuaSprite("bg", "sarvaBG/galaxy", -900, -500)
 	setScrollFactor("bg", 0.1, 0.1)
 	scaleObject('bg', 5, 5)
-	addGlitchEffect("bg", 2, 2, 0.04)
+	initLuaShader("TransparentLattice")
+	setSpriteShader("bg", "TransparentLattice")
 	addLuaSprite("bg", false)
+end
+
+function onUpdate(elapsed)
+	setShaderFloat('bg', 'iTime', getSongPosition()/800)
 end
